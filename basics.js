6807793f1cbf6bec4769d2a2b7ac55b07d6cd5b4
DarthBader-24 <baderbinsalem13@gmail.com>
2022-02-08 15:10:24 +0300

    badabada

---
+++ basics.js
@@ -0,0 +1,11 @@
+var array2 = [1,2,3,4,5,6]
+var x = 0
+// What I need to do?
+ for(i=0; i < array2.length; i++){
+ x+=array2[i]
+}
+
+ console.log(x)
+ // for (b=0; b < 15; b++ ){
+//     console.log(b)
+// }
